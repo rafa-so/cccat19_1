b7d91f312c39b3f5ca99b61a9bfc53ba2de28506
Rafael Oliveira <rafael.olveira@culturainglesa.com.br>
2025-01-09 19:23:14 -0300

    Criado calculo de distancia de forma inicial

--- backend/src/application/use_case/GetRide.ts
+++ backend/src/application/use_case/GetRide.ts
@@ -1,6 +1,7 @@
 import { AccountRepository } from "../repository/AccountRepository";
 import { RideRepository } from "../../infra/repository/RideRepository";
 import Account from "../../domain/Account";
+import Coord from "../../domain/vo/Coord";
 
 export default class GetRide {
 	constructor(
@@ -12,7 +13,13 @@ export default class GetRide {
 		const ride = await this.rideRepository.getRideById(rideId);
 		const passengerAccount: Account = await this.accountRepository.getAccountById(ride.getPassengerId());
 
-		console.log(ride);
+		let distance = 0;
+
+		for (const [index, position] of ride.positions.entries()) {
+			const nextPosition = ride.positions[index + 1];
+			if (!nextPosition) break;
+			distance += this.calculateDistance(position.getCoord(), nextPosition.getCoord());
+		}
 
 		return { 
 			rideId: ride.getRideId(),
@@ -24,10 +31,26 @@ export default class GetRide {
 			toLong: ride.getTo().getLong(),
 			status: ride.getStatus(),
 			fare: ride.fare,
-			distance: ride.distance,
+			distance,
 			date: ride.date, 
 			passengerName: passengerAccount.getName()
 		}
+	}
+
+	calculateDistance(from: Coord, to: Coord) {
+		const earthRadius = 6371;
+		const degreesToRadians = Math.PI / 180;
+		const deltaLat = (to.getLat() - from.getLat()) * degreesToRadians;
+		const deltaLon = (to.getLong() - from.getLong()) * degreesToRadians;
+		const a =
+			Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+			Math.cos(from.getLat() * degreesToRadians) *
+			Math.cos(to.getLat() * degreesToRadians) *
+			Math.sin(deltaLon / 2) *
+			Math.sin(deltaLon / 2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		const distance = earthRadius * c;
+		return Math.round(distance);
 	}
 }
 
