86dc3af526d7887cd13442a8a01fe25087996ebd
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2026-03-30 21:48:09 -0300

    feat: Criação de testes iniciais

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
+import { isExportDeclaration } from "typescript";
 
 test("Deve criar uma conta de passageiro", async function() {
+    // Given
     const input = {
         name: "John Doe",
         email: `john.doe${Math.random()}@gmail.com`,
@@ -9,5 +11,16 @@ test("Deve criar uma conta de passageiro", async function() {
         isPassenger: true,
     }
 
-    await axios.post("http://localhost:3000/signup", input);
+    // When
+    const responseSignup = await axios.post("http://localhost:3000/signup", input);
+    const outputSignup = responseSignup.data;
+    const responseGetAccount = await axios.get(`http://localhost:3000/account/${outputSignup.accountId}`);
+    const outputGetAccount = responseGetAccount.data;
+
+    // Then
+    expect(outputSignup.accountId).toBeDefined();
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.cpf).toBe(input.cpf);
+    expect(outputGetAccount.is_passenger).toBe(input.isPassenger);
 });
