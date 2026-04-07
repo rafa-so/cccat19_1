0e0ffce6dfef6020abd106ef496771341f31ae22
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2026-04-07 19:01:24 -0300

    refactor: Aplicadas técnicas de refatoração no método de signup

--- src/signup.ts
+++ src/signup.ts
@@ -11,24 +11,14 @@ app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
-		let result;
 		const [existingAccount] = await connection.query("select * from ccca.account where email = $1", [input.email]);
 		if (existingAccount) throw -4;
 		if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw -3;
 		if (!input.email.match(/^(.+)@(.+)$/)) throw -2;
 		if (!validateCpf(input.cpf)) throw -1;
-		if (input.isDriver) {
-			if (!input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw -5;
-			await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-			const obj = { accountId: id };
-			result = obj;
-		} else {
-			await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-			const obj = { accountId: id };
-			result = obj;
-		}
-
-		res.json(result);
+		if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw -5;
+		await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
+		res.json({ accountId: id });
 	} catch (e: any) {
 		res.status(422).json({ message: e });
 	} finally {
